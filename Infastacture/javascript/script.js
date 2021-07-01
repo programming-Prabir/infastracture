622d5592efe49323c3bc04fe4426e182442d53ff
Prabir Manidas <86470568+programming-Prabir@users.noreply.github.com>
2021-07-01 12:00:47 +0530

    Add files via upload

---
+++ Infastacture/javascript/script.js
@@ -0,0 +1,5 @@
+const navToggle = document.querySelector(".navToggle");
+const menu = document.querySelector(".menu");
+navToggle.addEventListener("click", function () {
+    menu.classList.toggle("Show");
+});
